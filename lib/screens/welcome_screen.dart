f024ccf9bac0cbff0348178aa655f721a9562c92
prsheth <pranayrsheth@gmail.com>
2020-04-16 14:05:05 -0400

    AnimationController added and enabled the class with the SingleTicker mixin, further added a listener to see controller value for a duration of 1 second and attached that to the state of the class. Added this state variable to the background color of the app

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -13,11 +13,32 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  //added mixin to provide extra capability to the class
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this, //required property to tell it which ticker to use
+    );
+
+    controller.forward();
+
+    //adding controller listener
+    controller.addListener(() {
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.red.withOpacity(controller.value),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
